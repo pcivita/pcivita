96cf0bce108a3ccb96e60843de380c6d1e3065fc
pcivita <pcivita@stanford.edu>
2024-03-15 17:12:55 -0700

    Build Ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import BentoGrid from "@/components/BentoGrid";
 
 export default function Home() {
   return (
@@ -10,19 +10,19 @@ export default function Home() {
           <div className="flex justify-center items-center ml-4 w-2/3">
             <div className="flex flex-col gap-5 border-black border-2 p-5">
               <h1 className="font-bold text-text-dark text-7xl border-2">
-                Hi, I'm{" "}
+                Hi, I`&apos;`m{" "}
                 <span className="text-background-light">Pedro Civita</span> I
                 blurb, blurb, and blurb.
               </h1>
               <div className="w-24 h-2 bg-purple my-4"></div>
               <h2 className="text-text-sub text-2xl border-2">
-                I’m a Stanford Masters Student Studying
+                I`&apos;`m a Stanford Masters Student Studying
                 <span className="text-text-dark font-bold">
                   {" "}
                   Human Computer Interaction{" "}
                 </span>
-                and I love it a lot lot lot lot lot it's just the best thing
-                ever.
+                and I love it a lot lot lot lot lot it`&apos;`s just the best
+                thing ever.
               </h2>
             </div>
           </div>
@@ -32,7 +32,7 @@ export default function Home() {
             <div className="rounded-full overflow-hidden border-8 border-background-light ">
               <img
                 src="/profilePic.jpg"
-                alt="Person's Name"
+                alt="Pedro Civita"
                 className="object-cover"
                 style={{
                   height: undefined,
@@ -44,7 +44,7 @@ export default function Home() {
         </div>
       </div>
       <div className="bg-background-light h-1/2 w-full">
-        <h1>Next </h1>
+        <BentoGrid />
       </div>
     </div>
   );
---
+++ components/BentoGrid.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function BentoGrid() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-200">
+      <div className="grid h-full w-2/3 grid-cols-4 grid-rows-6 gap-4 bg-slate-200">
+        <Link
+          href={"/Muse"}
+          className={`col-span-2 row-span-4 rounded-xl  border-slate-400/10 bg-neutral-100 
+          dark:bg-neutral-900`}
+        >
+          <motion.div
+            whileHover={{ scale: 1.02 }}
+            className="relative h-full w-full overflow-hidden rounded-lg bg-white "
+          >
+            <Image
+              src={"/Images/muse.svg"}
+              alt="hell"
+              layout="fill"
+              objectFit="contain"
+              className="p-4"
+            />
+          </motion.div>
+        </Link>
+
+        <Link
+          href={"/Muse"}
+          className={`col-span-2 row-span-2 rounded-xl  border-slate-400/10 bg-neutral-100 
+          dark:bg-neutral-900`}
+        >
+          <motion.div
+            whileHover={{ scale: 1.02 }}
+            className="relative h-full w-full overflow-hidden rounded-lg  bg-white"
+          >
+            <Image
+              src={"/Images/turno.svg"}
+              alt="hell"
+              layout="fill"
+              objectFit="contain"
+              className="p-4"
+            />
+          </motion.div>
+        </Link>
+        <Link
+          href={"/Muse"}
+          className={`col-span-2 row-span-2 rounded-xl  border-slate-400/10 bg-neutral-100 
+          dark:bg-neutral-900`}
+        >
+          <motion.div
+            whileHover={{ scale: 1.02 }}
+            className="relative h-full w-full overflow-hidden rounded-lg  bg-white"
+          >
+            <Image
+              src={"/Images/dfg.png"}
+              alt="hell"
+              layout="fill"
+              objectFit="contain"
+              className="p-4"
+            />
+          </motion.div>
+        </Link>
+        {/* <div
+          className={`col-span-2 row-span-2 rounded-xl  border-slate-400/10 bg-neutral-100 p-4
+          dark:bg-neutral-900`}
+        >
+          Hello{" "}
+        </div> */}
+        {/* <div
+          className={`col-span-3 row-span-2 rounded-xl  border-slate-400/10 bg-neutral-100 p-4
+          dark:bg-neutral-900`}
+        >
+          BNA{" "}
+        </div>
+        <div
+          className={`col-span-1 row-span-2 rounded-xl  border-slate-400/10 bg-neutral-100 p-4
+          dark:bg-neutral-900`}
+        >
+          VIDEO GAMES{" "}
+        </div> */}
+      </div>
+    </div>
+  );
+}
