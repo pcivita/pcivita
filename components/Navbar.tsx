db506c977748b4840a2dc1179179b6ffda973e6f
pcivita <pcivita@stanford.edu>
2024-03-28 08:40:24 +0900

    HEADER START

---
+++ components/Navbar.tsx
@@ -0,0 +1,149 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+const navLinks = [
+  { title: "What we do", href: "/" },
+  { title: "How it works", href: "/" },
+  { title: "Case studies", href: "/" },
+  { title: "About", href: "/" },
+  { title: "Contact", href: "/" },
+];
+// Define the types for the props
+type NavbarProps = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Navbar: React.FC<NavbarProps> = ({ open, setOpen }) => {
+  const toggleMenu = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+  const menuVars = {
+    initial: {
+      scaleY: 0,
+    },
+    animate: {
+      scaleY: 1,
+      transition: {
+        duration: 0.5,
+        ease: [0.12, 0, 0.39, 0],
+      },
+    },
+    exit: {
+      scaleY: 0,
+      transition: {
+        delay: 0.5,
+        duration: 0.5,
+        ease: [0.22, 1, 0.36, 1],
+      },
+    },
+  };
+  const containerVars = {
+    initial: {
+      transition: {
+        staggerChildren: 0.09,
+        staggerDirection: -1,
+      },
+    },
+    open: {
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.09,
+        staggerDirection: 1,
+      },
+    },
+  };
+
+  return (
+    <header>
+      <nav className="flex justify-between items-center py-8 lg:py-4 px-2 z-50">
+        <div
+          className="cursor-pointer lg:hidden text-md text-black"
+          onClick={toggleMenu}
+        >
+          Menu
+        </div>
+      </nav>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            variants={menuVars}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            className="fixed left-0 top-0 w-full h-screen origin-top bg-yellow-400 text-black p-10"
+          >
+            <div className="flex h-full flex-col">
+              <div className="flex justify-between">
+                <h1 className="text-lg text-black">Portfolio</h1>
+                <p
+                  className="cursor-pointer text-md text-black"
+                  onClick={toggleMenu}
+                >
+                  Close
+                </p>
+              </div>
+              <motion.div
+                variants={containerVars}
+                initial="initial"
+                animate="open"
+                exit="initial"
+                className="flex flex-col h-full justify-center font-lora items-center gap-4 "
+              >
+                {navLinks.map((link, index) => {
+                  return (
+                    <div className="overflow-hidden" key={index}>
+                      <MobileNavLink
+                        key={index}
+                        title={link.title}
+                        href={link.href}
+                      />
+                    </div>
+                  );
+                })}
+              </motion.div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </header>
+  );
+};
+
+export default Navbar;
+const mobileLinkVars = {
+  initial: {
+    y: "30vh",
+    transition: {
+      duration: 0.5,
+      ease: [0.37, 0, 0.63, 1],
+    },
+  },
+  open: {
+    y: 0,
+    transition: {
+      ease: [0, 0.55, 0.45, 1],
+      duration: 0.7,
+    },
+  },
+};
+// Define a type for the props that MobileNavLink will receive
+type MobileNavLinkProps = {
+  title: string;
+  href: string;
+};
+
+// The MobileNavLink component with properly typed props
+const MobileNavLink: React.FC<MobileNavLinkProps> = ({ title, href }) => {
+  return (
+    <motion.div
+      variants={mobileLinkVars}
+      className="text-5xl uppercase text-black"
+    >
+      {/* Using an anchor tag <a> as a child of Link for proper navigation */}
+      <Link href={href}>{title}</Link>
+    </motion.div>
+  );
+};
